26aa67834525e7435880200808691e140f1913a7
jerry <jay_wcom@163.com>
2018-12-06 04:45:11 +0000

    add paramter LISTEN_PORT ,OPENSDS_HOST_IP for dashborad images

--- image_builder.sh
+++ image_builder.sh
@@ -24,24 +24,3 @@ ng build --prod
 
 cp -R ./dist/* /var/www/html/
 
-cat > /etc/nginx/sites-available/default <<EOF
-    server {
-        listen 8088 default_server;
-        listen [::]:8088 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        location /v3/ {
-            proxy_pass http://127.0.0.1/identity/v3/;
-        }
-
-        location /v1beta/ {
-            proxy_pass http://127.0.0.1:50040/v1beta/;
-        }
-
-        location /v1/ {
-            proxy_pass http://127.0.0.1:8089/v1/;
-            client_max_body_size 10240m;
-        }
-    }
-EOF
