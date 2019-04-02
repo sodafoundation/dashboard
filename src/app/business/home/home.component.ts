3fcdf8e29bb5fa0c909f10d5e38c4444149a10e2
yuejuSDS <1094512433@qq.com>
2019-04-02 15:29:34 +0800

    Add an AK/SK prompt to the bucket page

--- src/app/business/home/home.component.ts
+++ src/app/business/home/home.component.ts
@@ -156,50 +156,55 @@ export class HomeComponent implements OnInit {
             window['canonicalString'](requestMethod, url, ()=>{
                 let options: any = {};
                 this.getSignature(options);
-                this.BucketService.getBuckets(options).subscribe((res)=>{
-                    let str = res['_body'];
-                    let x2js = new X2JS();
-                    let jsonObj = x2js.xml_str2json(str);
-                    let buckets = (jsonObj ? jsonObj.ListAllMyBucketsResult.Buckets:[]);
-                    let allBuckets = [];
-                    if(Object.prototype.toString.call(buckets) === "[object Array]"){
-                        allBuckets = buckets;
-                    }else if(Object.prototype.toString.call(buckets) === "[object Object]"){
-                        allBuckets = [buckets];
-                    }
-                    this.counts.bucketsCount = allBuckets.length;
-                    Consts.BUCKET_BACKND.clear();
-                    Consts.BUCKET_TYPE.clear();
-                    this.http.get('v1/{project_id}/backends').subscribe((res)=>{
-                        let backends = res.json().backends ? res.json().backends :[];
-                        let backendsObj = {};
-                        backends.forEach(element => {
-                            backendsObj[element.name]= element.type;
+                if(Object.keys(options).length > 0 ){
+                    this.BucketService.getBuckets(options).subscribe((res)=>{
+                        let str = res['_body'];
+                        let x2js = new X2JS();
+                        let jsonObj = x2js.xml_str2json(str);
+                        let buckets = (jsonObj ? jsonObj.ListAllMyBucketsResult.Buckets:[]);
+                        let allBuckets = [];
+                        if(Object.prototype.toString.call(buckets) === "[object Array]"){
+                            allBuckets = buckets;
+                        }else if(Object.prototype.toString.call(buckets) === "[object Object]"){
+                            allBuckets = [buckets];
+                        }
+                        this.counts.bucketsCount = allBuckets.length;
+                        Consts.BUCKET_BACKND.clear();
+                        Consts.BUCKET_TYPE.clear();
+                        this.http.get('v1/{project_id}/backends').subscribe((res)=>{
+                            let backends = res.json().backends ? res.json().backends :[];
+                            let backendsObj = {};
+                            backends.forEach(element => {
+                                backendsObj[element.name]= element.type;
+                            });
+                            allBuckets.forEach(item=>{
+                                Consts.BUCKET_BACKND.set(item.Name,item.LocationConstraint);
+                                Consts.BUCKET_TYPE.set(item.Name,backendsObj[item.LocationConstraint]);
+                            });
+                            this.initBackendsAndNum(backends);//must after Consts.BUCKET_BACKND.set
                         });
-                        allBuckets.forEach(item=>{
-                            Consts.BUCKET_BACKND.set(item.Name,item.LocationConstraint);
-                            Consts.BUCKET_TYPE.set(item.Name,backendsObj[item.LocationConstraint]);
-                        });
-                        this.initBackendsAndNum(backends);//must after Consts.BUCKET_BACKND.set
-                    });
-                });  
+                    }); 
+                }
+                 
             });
         }) 
     }
     //Request header with AK/SK authentication added
     getSignature(options) {
         let SignatureObjectwindow = window['getSignatureKey']();
-        let kAccessKey = SignatureObjectwindow.SignatureKey.AccessKey;
-        let kDate = SignatureObjectwindow.SignatureKey.dateStamp;
-        let kRegion = SignatureObjectwindow.SignatureKey.regionName;
-        let kService = SignatureObjectwindow.SignatureKey.serviceName;
-        let kSigning = SignatureObjectwindow.kSigning;
-        let Credential = kAccessKey + '/' + kDate.substr(0,8) + '/' + kRegion + '/' + kService + '/' + 'sign_request';
-        let Signature = 'OPENSDS-HMAC-SHA256' + ' Credential=' + Credential + ',SignedHeaders=host;x-auth-date' + ",Signature=" + kSigning;
-        options['headers'] = new Headers();
-        options.headers.set('Authorization', Signature);
-        options.headers.set('X-Auth-Date', kDate);
-        return options;  
+        if(Object.keys(SignatureObjectwindow.SignatureKey).length > 0){
+            let kAccessKey = SignatureObjectwindow.SignatureKey.AccessKey;
+            let kDate = SignatureObjectwindow.SignatureKey.dateStamp;
+            let kRegion = SignatureObjectwindow.SignatureKey.regionName;
+            let kService = SignatureObjectwindow.SignatureKey.serviceName;
+            let kSigning = SignatureObjectwindow.kSigning;
+            let Credential = kAccessKey + '/' + kDate.substr(0,8) + '/' + kRegion + '/' + kService + '/' + 'sign_request';
+            let Signature = 'OPENSDS-HMAC-SHA256' + ' Credential=' + Credential + ',SignedHeaders=host;x-auth-date' + ",Signature=" + kSigning;
+            options['headers'] = new Headers();
+            options.headers.set('Authorization', Signature);
+            options.headers.set('X-Auth-Date', kDate);
+            return options; 
+        } 
     }
     initBackendsAndNum(backends){
         let backendArr = Array.from(Consts.BUCKET_BACKND.values());
