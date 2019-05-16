9aaea31e2c8a07aedb1eacee5763e58d0ba3af04
anvithks <anvith.k.s@huawei.com>
2019-05-16 19:09:08 +0530

    Updated the dynamic form component. Added underscore lib to the angular-cli.json

--- src/app/business/services/dynamic-form/dynamic-form.component.ts
+++ src/app/business/services/dynamic-form/dynamic-form.component.ts
@@ -1,6 +1,6 @@
 import { Router,ActivatedRoute } from '@angular/router';
 import { Component, OnInit, Input } from '@angular/core';
-import { I18NService, MsgBoxService, Utils } from '../../../shared/api';
+import { I18NService, MsgBoxService, Utils, ParamStorService } from '../../../shared/api';
 import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { WorkflowService } from '../workflow.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
@@ -15,6 +15,14 @@ export class DynamicFormComponent implements OnInit {
   @Input() dataObject: any;
   objectProps: any;
   form: FormGroup;
+  requestBody : any;
+  default_parameters: any = {
+    "profile_id": "",
+    "tenant_id": "",
+    "auth": "",
+    "token": "",
+    "user":""
+  };
   options = {
     headers: {
         'X-Auth-Token': localStorage['auth-token']
@@ -25,6 +33,7 @@ export class DynamicFormComponent implements OnInit {
     private ActivatedRoute:ActivatedRoute,
     public I18N: I18NService,
     public wfservice: WorkflowService,
+    public paramStor: ParamStorService,
     private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -34,9 +43,19 @@ export class DynamicFormComponent implements OnInit {
         .map(prop => {
           return Object.assign({}, { key: prop} , this.dataObject[prop]);
         });
-
+      
+        _.each(this.dataObject, function(item){
+          if(item['required']==true){
+            item['validation'] = {"required": true};
+          }
+        })
       console.log("Updated Object:DataObject", this.objectProps, this.dataObject);
 
+      this.requestBody = _.pick(this.objectProps, function(item){
+          item['validation'] = {required: true};
+          return item['required']==true;
+      });
+      console.log("Request Body:", this.requestBody);
       _.each(this.objectProps, function(item){
         item['label'] = item['key'].replace(/_/g, ' ').replace(/(?: |\b)(\w)/g, function(key, p1) {
               return key.toUpperCase();    
@@ -64,12 +83,20 @@ export class DynamicFormComponent implements OnInit {
 
       // setup the form
       const formGroup = {};
+
+     /*  _.each(this.objectProps, function(item, key, val){
+        formGroup[key] = new FormControl(val[key] || '', this.mapValidators(item['validation']))
+        console.log("Interim formGroup:", formGroup);
+      });
+      this.form = new FormGroup(formGroup); */
+      
       for(let prop of Object.keys(this.dataObject)) {
         formGroup[prop] = new FormControl(this.dataObject[prop].value || '', this.mapValidators(this.dataObject[prop].validation));
       }
-
-        this.form = new FormGroup(formGroup);
-      }
+      this.form = new FormGroup(formGroup);
+      console.log("New FormGroup:", this.form);
+    }
+      
 
       mapValidators(validators) {
         const formValidators = [];
