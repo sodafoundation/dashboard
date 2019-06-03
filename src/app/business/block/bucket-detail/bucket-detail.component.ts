f9520e91a92da9d93faa1e5a88b84d1d515db4b7
yuejuSDS <1094512433@qq.com>
2019-06-03 10:55:54 +0800

    Modify the lifeCycle interface problem

--- src/app/business/block/bucket-detail/bucket-detail.component.ts
+++ src/app/business/block/bucket-detail/bucket-detail.component.ts
@@ -228,6 +228,11 @@ export class BucketDetailComponent implements OnInit {
           })
           this.allDir.forEach((item,index)=>{
             this.allDir[index].Tier = "Tier_" + item.Tier + " (" + item.StorageClass + ")";
+            if(item.objectName.length > 20){
+              item['name'] = item.objectName.substr(0,8) + "...";
+            }else{
+              item['name'] = item.objectName;
+            }
           });
         });
         })
