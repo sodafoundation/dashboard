7de1498522bc600544b3e306be0d494d22c0d12c
Anvith K.S <anvith3@gmail.com>
2020-09-10 23:02:49 +0530

    Fixed CI error. Added missing method formatBytes

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -119,6 +119,20 @@ export class Utils {
         ret = this.numberFormat(ret, decimals);
         return ret + " " + unit;
     }
+    /***
+     * returns the capacity value in appropriate display units
+     */
+    static formatBytes(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+    
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+    
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
 
     /**
      * Help get validation information.
