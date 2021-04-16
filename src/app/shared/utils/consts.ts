61a977a95b6c1842c8ad3efb25e7ce1978681369
click2cloud-hebe <dhanshree.kukekar@click2cloud.net>
2021-04-16 14:42:31 +0530

    dhanshree:soda dashboard-ui for sony-oda

--- src/app/shared/utils/consts.ts
+++ src/app/shared/utils/consts.ts
@@ -73,12 +73,12 @@ export const Consts = {
         'alibaba-oss' : "Alibaba Object Storage",
         'sony-oda':"Sony ODA"
     },
-    SODA_HOST_IP: '192.168.200.169',
+    SODA_HOST_IP: '',
     SODA_GRAFANA_PORT: '',
     SODA_ALERTMANAGER_PORT: '',
     SODA_PROMETHEUS_PORT: '',
-    S3_HOST_IP: '192.168.200.169',
-    S3_HOST_PORT: '8090',
+    S3_HOST_IP: '',
+    S3_HOST_PORT: '',
     STORAGE_CLASSES: {
         'aws-s3' : [
             {
