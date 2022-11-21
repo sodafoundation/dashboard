1c3263bc2fc8f94a5520d12ac210b47a76e97f2b
Joseph Vazhappilly <josephvp@gmail.com>
2022-11-21 15:47:53 +0530

    Add support for MicroSAN storage

--- src/app/shared/utils/consts.ts
+++ src/app/shared/utils/consts.ts
@@ -214,6 +214,10 @@ export const Consts = {
                 value: 'inspur'
             },
             {
+                label: "MacroSAN",
+                value: 'macrosan'
+            },
+            {
                 label: "NetApp",
                 value: 'netapp'
             },
@@ -227,11 +231,11 @@ export const Consts = {
             }
         ],
         resources:{
-            volumes : ['fake_driver', 'vmax', 'pmax', 'unity', 'vnx_block', 'vplex', 'oceanstor', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'eternus', 'flasharray', 'msa', 'ds8k'],
-            pools : ['fake_driver', 'vmax', 'pmax', 'unity', 'vnx_block', 'vplex', 'oceanstor', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'eternus', 'msa', 'hnas', 'ds8k'],
-            controllers : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', 'vplex', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'eternus', 'flasharray', 'msa', 'hnas', 'ds8k'],
-            ports : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', 'vplex', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'eternus', 'flasharray', 'msa', 'hnas', 'ds8k'],
-            disks : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'eternus', 'flasharray', 'msa', 'hnas'],
+            volumes : ['fake_driver', 'vmax', 'pmax', 'scaleio', 'unity', 'vnx_block', 'vplex', 'oceanstor', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'eternus', 'flasharray', 'msa', 'ds8k', 'macrosan'],
+            pools : ['fake_driver', 'vmax', 'pmax', 'scaleio', 'unity', 'vnx_block', 'vplex', 'oceanstor', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'eternus', 'msa', 'hnas', 'ds8k', 'macrosan',],
+            controllers : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', 'vplex', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'scaleio', 'eternus', 'flasharray', 'msa', 'hnas', 'ds8k', 'macrosan'],
+            ports : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', 'vplex', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'scaleio', 'eternus', 'flasharray', 'msa', 'hnas', 'ds8k', 'macrosan'],
+            disks : ['fake_driver', 'oceanstor', 'unity', 'vnx_block', '3par', 'unistor_cf', 'primera', 'vsp', 'storwize_svc', 'as5500', 'cmode', 'vmax', 'pmax', 'scaleio', 'eternus', 'flasharray', 'msa', 'hnas', 'macrosan'],
             qtrees : ['fake_driver', 'oceanstor', 'unity', 'cmode', 'hnas'],
             filesystems : ['fake_driver', 'oceanstor', 'unity', 'cmode', 'hnas'],
             shares: ['fake_driver', 'oceanstor', 'unity', 'cmode', 'hnas'],
@@ -256,6 +260,28 @@ export const Consts = {
                     label: "VMAX",
                     value: {
                         name: 'vmax',
+                        rest: true,
+                        ssh: false,
+                        cli: false,
+                        smis: false,
+                        extra: true
+                    }
+                },
+                {
+                    label: "PowerMAX",
+                    value: {
+                        name: 'pmax',
+                        rest: true,
+                        ssh: false,
+                        cli: false,
+                        smis: false,
+                        extra: true
+                    }
+                },
+                {
+                    label: "ScaleIO",
+                    value: {
+                        name: 'scaleio',
                         rest: true,
                         ssh: false,
                         cli: false,
@@ -294,17 +320,6 @@ export const Consts = {
                         cli: false,
                         smis: false,
                         extra: false
-                    }
-                },
-                {
-                    label: "PMAX",
-                    value: {
-                        name: 'pmax',
-                        rest: true,
-                        ssh: false,
-                        cli: false,
-                        smis: false,
-                        extra: true
                     }
                 }
             ],
@@ -422,6 +437,19 @@ export const Consts = {
                     label: "AS5500/AS5300/AS2600/AS2200",
                     value: {
                         name: 'as5500',
+                        rest: false,
+                        ssh: true,
+                        cli: false,
+                        smis: false,
+                        extra: false
+                    }
+                }
+            ],
+            'macrosan' : [
+                {
+                    label: "MacroSAN",
+                    value: {
+                        name: 'macrosan',
                         rest: false,
                         ssh: true,
                         cli: false,
